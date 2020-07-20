2e2e24fec5fb9f9e9f0666eecb0076898882b6de
*Althaf <althafmuhammad@computer.org>
2020-07-20 14:03:55 +0530

    Updates

---
+++ precache-manifest.c25846b41559192dd4a687dda9441d22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35757ed984368f12bec5e719a5c9117d",
+    "url": "/Netflix-Clone/index.html"
+  },
+  {
+    "revision": "f61c38681dc7a940ecf6",
+    "url": "/Netflix-Clone/static/css/main.8549b9da.chunk.css"
+  },
+  {
+    "revision": "98a7e68cf67706de8313",
+    "url": "/Netflix-Clone/static/js/2.770c3cac.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/Netflix-Clone/static/js/2.770c3cac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f61c38681dc7a940ecf6",
+    "url": "/Netflix-Clone/static/js/main.0219a59d.chunk.js"
+  },
+  {
+    "revision": "b2b8b24758047419b104",
+    "url": "/Netflix-Clone/static/js/runtime-main.f28e37b4.js"
+  }
+]);
